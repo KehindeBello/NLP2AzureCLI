71147cead494a4691acbf348574a4b73348100b1
kehindebello <adedotunomomeji@gmail.com>
2024-06-30 13:47:11 +0100

    add: helper/utility functions

---
+++ resources/utils.py
@@ -0,0 +1,14 @@
+import bcrypt
+
+def hashPassword(password):
+    #encode password
+    encoded_password = password.encode("utf-8")
+    #salt 
+    salt = bcrypt.gensalt()
+    #hashing the password
+    hash = bcrypt.hashpw(encoded_password, salt=salt)
+    return hash
+
+def checkPassword(password, hashed_password):
+    encoded_password = password.encode("utf-8")
+    return bcrypt.checkpw(password=encoded_password, hashed_password=hashed_password)
