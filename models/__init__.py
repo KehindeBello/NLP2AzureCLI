9e9c4023df635f13e208c9bb87630bdbb907c9a4
kehindebello <adedotunomomeji@gmail.com>
2024-06-30 13:43:04 +0100

    add: db models for user and azure-cli credentials

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+
+from models.user import UserModel
+from models.azurecredentials import AzureCredentialModel
---
+++ models/user.py
@@ -0,0 +1,22 @@
+from db import db
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    azure_credentials = db.relationship("AzureCredentialModel", back_populates="user", lazy="dynamic",cascade="all, delete-orphan")
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email = email).first()
+    
+
+
+
