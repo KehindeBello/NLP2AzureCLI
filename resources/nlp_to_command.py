5cf3940fc149c4e492d15b4f1f56ea6d283988e9
kehindebello <adedotunomomeji@gmail.com>
2024-07-20 16:26:54 +0100

    ignore warnings

--- resources/nlp_to_command.py
+++ resources/nlp_to_command.py
@@ -1,12 +1,14 @@
 import os
+import warnings
 from dotenv import load_dotenv
 from langchain_openai import OpenAI
-from langchain_community.llms import openai
-from langchain.chains import LLMChain
+from langchain.chains.llm import LLMChain
 from langchain.prompts import PromptTemplate
 
 # Set up the OpenAI API key. Load environmental variables
 load_dotenv()
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # Initialize the OpenAI LLM
 llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -30,7 +32,8 @@ def translate_to_azure_cli(instruction):
     azure_cli_command = chain.run(instruction)
     return azure_cli_command
 
+
 # Example usage
-# instruction = "Get a list of resource groups."
-# azure_cli_command = translate_to_azure_cli(instruction)
-# print("Azure CLI Command:", azure_cli_command)
+instruction = "Get a list of resource groups."
+azure_cli_command = translate_to_azure_cli(instruction)
+print("Azure CLI Command:", azure_cli_command)
