97df257d6024af9b870bafd6fea5ecbadd938ee7
kehindebello <adedotunomomeji@gmail.com>
2024-08-03 09:12:16 +0100

    add: azure imports

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,3 +1,3 @@
 from resources.user import UserLogin, UserRegister, UserLogout, ForgotPassword, ResetPassword
 from resources.process_nlp import ConvertNLP
-from resources.azure_auth import AuthAzure, RunCommand
+from resources.azure_auth import AuthAzure, RunCommand, AzureCliResponse
